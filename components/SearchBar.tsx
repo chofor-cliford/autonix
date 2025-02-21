54174bbb7268d1449fbc55329f24efed9e031ae7
Chofor Cliford <betterfuture032@gmail.com>
2025-02-21 13:49:52 -0500

    Add SearchBar and CustomerFilter components; update Hero and Footer layout

---
+++ components/SearchBar.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+const SearchBar = () => {
+  const handleSubmit = () => {};
+
+  return (
+    <form className="searchbar" onSubmit={handleSubmit}>
+      <div className="searchbar__item">
+        
+      </div>
+    </form>
+  )
+}
+
+export default SearchBar
